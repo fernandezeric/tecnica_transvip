7a658155f45ae5e45be4a287869ee0325801c59b
fernandezeric <ericfernandezgonzalez@gmail.com>
2024-03-21 15:17:41 -0300

    gets

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,4 +1,4 @@
-import { type RestaurantWithoutMenu, type MenuWithRestaurantName, type Menu } from "../types/api"
+import { type RestaurantWithoutMenu, type MenuWithRestaurantName, type Menu } from '../types/api'
 
 const BASE_PATH = 'http://localhost:3000'
 const ORIGIN_URL = 'https://localhost:4321'
@@ -51,3 +51,67 @@ export const getAllMenusByRestaurantId = async ({ id }: { id: string }) => {
 
   return restaurants
 }
+
+export const getMenuByRestaurantIdAndMenuId = async ({ restaurantId, menuId }: { restaurantId: string, menuId: string }) => {
+  const rest = await fetch(`${BASE_PATH}/restaurants/${restaurantId}/menus/${menuId}`, {
+    method: 'GET',
+    headers: {
+      'Origin': `${ORIGIN_URL}`
+    }
+  })
+  const restaurants = await rest.json() as Menu
+
+  return restaurants
+}
+
+/**
+  {
+    "name": "Café Neruda (diagonal)",
+    "description": "Especialista en café",
+    "phone": "+56212666678",
+    "openTime": "13:00:00",
+    "closeTime": "3:00:00",
+    "rate": 9.9
+  }
+ */
+export const createRestaurant = async ({ data }: { data: any }) => {
+  const rest = await fetch(`${BASE_PATH}/restaurants/`, {
+    method: 'POST',
+    headers: {
+      'Origin': `${ORIGIN_URL}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  const restaurants = await rest.json()
+
+  return restaurants
+}
+
+
+/**
+  {
+    "name": "menú de día domingo",
+    "description": "para relajarse un día domingo",
+    "dishes": [
+    {
+        "name": "Ensalada de Quínoa y Palta",
+        "price": 5500
+      }
+    ]
+  }
+ */
+  export const createMenuByRestaurantId = async ({ id, data }: { id: string, data: any }) => {
+    const rest = await fetch(`${BASE_PATH}/restaurants/${id}/menus`, {
+      method: 'POST',
+      headers: {
+        'Origin': `${ORIGIN_URL}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    const restaurants = await rest.json()
+  
+    return restaurants
+  }
+  
